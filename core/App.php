9f27a643ed9a9898b957bbe0cd53d2d1cb2e0e1c
Nguyễn Văn Thương <80138135+ThuongNguyenVan158@users.noreply.github.com>
2022-04-19 22:12:34 +0700

    Update App.php

--- core/App.php
+++ core/App.php
@@ -9,41 +9,21 @@ class App{
  
         $arr = $this->UrlProcess();
         // Controller
-        //use when test local
-		// if(isset($arr))
-        // if( file_exists("./controllers/".$arr[0].".php") ){
-        //     $this->controller = $arr[0];
-        //     unset($arr[0]);
-        // }
-        // require_once "./controllers/". $this->controller .".php";
-        // $this->controller = new $this->controller;
-
-        // // Action
-		// if(isset($arr))
-        // if(isset($arr[1])){
-        //     if( method_exists( $this->controller , $arr[1]) ){
-        //         $this->action = $arr[1];
-        //     }
-        //     unset($arr[1]);
-        // }
-
-        //use when deploy, 
-        if(isset($arr))
-        if( file_exists("./controllers/".$arr[1].".php") ){
-            $this->controller = $arr[1];
-            unset($arr[0]);
-            unset($arr[1]);
+	if(isset($arr))
+        if( file_exists("./controllers/".$arr[0].".php") ){
+        	$this->controller = $arr[0];
+        	unset($arr[0]);
         }
         require_once "./controllers/". $this->controller .".php";
         $this->controller = new $this->controller;
 
-        // Action
-		if(isset($arr))
+        // // Action
+	if(isset($arr))
         if(isset($arr[1])){
-            if( method_exists( $this->controller , $arr[2]) ){
-                $this->action = $arr[2];
-            }
-            unset($arr[2]);
+        	if( method_exists( $this->controller , $arr[1]) ){
+        		$this->action = $arr[1];
+        	}
+        	unset($arr[1]);
         }
         // Params
         $this->params = $arr?array_values($arr):[];
