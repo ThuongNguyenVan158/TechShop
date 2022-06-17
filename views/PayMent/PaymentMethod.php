0c788aaebf5e108bae9ccbe5ef73f155ae24bf01
Nguyễn Văn Thương <thuong.nguyen@deliveree.com>
2022-06-18 01:35:10 +0700

    Update PaymentMethod.php

--- views/PayMent/PaymentMethod.php
+++ views/PayMent/PaymentMethod.php
@@ -168,8 +168,7 @@
                                             </table>
                                             <form action="/Payment/PaymentMethod" method="post">
                                                 <div class="text-center" id="momo-QR-code">
-                                                    <!-- <img id="momo-QR-code" src="/images/momo_success.jpg" -->
-                                                    <img id="momo-QR-code" src="https://drive.google.com/file/d/1QvcOSx8jRW6G_jIOxkE5esuOpdnzJj5y/view"
+                                                    <img id="momo-QR-code" src="/assets/images/momo_success.jpg"
                                                          alt='Pay Momo wallet success' />
                                                     <button class="finish-payment"
                                                             value="Ví điện tử Momo"
@@ -179,7 +178,7 @@
                                                     </button>
                                                 </div>
                                                 <div class="text-center" id="zalopay-QR-code">
-                                                    <img id="zalopay-QR-code" src="/images/zalopay_success.jpg"
+                                                    <img id="zalopay-QR-code" src="/assets/images/zalopay_success.jpg"
                                                          alt='Pay ZaloPay wallet success' />
                                                     <button class="finish-payment"
                                                             value="Ví điện tử ZaloPay"
@@ -189,7 +188,7 @@
                                                     </button>
                                                 </div>
                                                 <div class="text-center" id="airpay-QR-code">
-                                                    <img id="airpay-QR-code" src="/images/airpay_success.jpg"
+                                                    <img id="airpay-QR-code" src="/assets/images/airpay_success.jpg"
                                                          alt='Pay Airpay wallet success' />
                                                     <button class="finish-payment"
                                                             value="Ví điện tử AirPay"
