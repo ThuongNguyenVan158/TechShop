346d37af9c9e0f61d12e5b7a89398b1a0f7ae6e2
Nguyễn Văn Thương <thuong.nguyen@deliveree.com>
2022-06-19 10:14:30 +0700

    Update introduce.php

--- views/Home/introduce.php
+++ views/Home/introduce.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="/TechShop//assets/css/introduce.css">
+        <link rel="stylesheet" type="text/css" href="/assets/css/introduce.css">
         
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
